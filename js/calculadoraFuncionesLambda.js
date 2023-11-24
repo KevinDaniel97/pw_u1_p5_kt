c67b7176963d914d290e09841ff2a4486f5b89a2
carolf10 <fcarofialloscheca@hotmail.com>
2023-11-23 19:41:06 -0500

    taller8

--- js/calculadoraFuncionesLambda.js
+++ js/calculadoraFuncionesLambda.js
@@ -44,12 +44,11 @@ const dividirNumero = () => {
     return resultado;
 
 }
-constcambiarImagen = () => {
-
-    var l1 = document.getElementById('idimagen1').src = 'https://w7.pngwing.com/pngs/680/842/png-transparent-mario-bross-thumbnail.png" id="imagen2'
+const cambiarImagen = () => {
+    var l1 = document.getElementById('idimagen2').src = 'https://www.nintenderos.com/wp-content/uploads/2009/08/luigi-nintendo.jpg'
 
 }
-const parsear = (id) => parent(document.getElementById(id).value)
+const parsear = (id) => parseInt(document.getElementById(id).value)
 
 function conceptosJS() {
     let var1 = 10;
@@ -61,14 +60,125 @@ function conceptosJS() {
     var var2 = 10;
     var var2Texto = 'Texto2';
     console.log(var2);
-    console.log(var2Text2);
+    console.log(var2Texto);
 
     const const1 = 10;
     const const1Texto = 'Texto3';
     console.log(const1);
-    console.log(conts1Texto);
+    console.log(const1Texto);
 
     //declaracion 
     const diasSemana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
     console.log(diasSemana)
+    console.log(diasSemana[0])
+    console.log(diasSemana[4])
+    console.log(diasSemana[5])
+    diasSemana[5] = "Sabado"
+    console.log(diasSemana[5])
+    diasSemana.push("Domingo")
+    console.log(diasSemana)
+    diasSemana.unshift("feriado")
+    console.log(diasSemana)
+    const edades = [1, 2, 3, 4, 5]
+    console.log(edades)
+    const edades2 = [6, 7, 8]
+    const edadesFinales = edades.concat(edades2)
+    console.log(edadesFinales)
+
+
+    for (const dia of diasSemana) {
+        console.log(dia)
+        if (dia === "Martes") {
+            console.log('martes dos por uno')
+
+        }
+    }
+
+    // declaracion de objetos
+    const estudiante = {
+        nombre: "Kevin",
+        apellido: "Toapanta",
+        edad: 34,
+        genero: "M",
+        ciudad: "Quito"
+    }
+    console.log(estudiante)
+    console.log(estudiante.apellido)
+    estudiante.nombre = "Daniel"
+    console.log(estudiante.nombre)
+
+    const persona = {
+        nombre: "Kevin",
+        apellido: "Toapanta",
+        edad: 34,
+        direccion: {
+            callePrincipal: "America",
+            calleSecundaria: "Bolivia",
+            numeracion: "Oe3-101",
+            barrio: "Mariana de Jesus"
+        }
+    }
+    console.log(persona)
+    console.log(persona.direccion.callePrincipal)
+
+    //arreglo de 4 pacientes
+    const pacientes =
+        [
+            {
+                nombre: "Kevin1",
+                apellido: "Toapanta1",
+                edad: 24,
+            },
+            {
+                nombre: "Kevin2",
+                apellido: "Toapanta2",
+                edad: 25,
+            },
+            {
+                nombre: "Kevin3",
+                apellido: "Toapanta3",
+                edad: 26,
+            },
+            {
+                nombre: "Kevin4",
+                apellido: "Toapanta4",
+                edad: 25,
+            }
+        ]
+
+    console.log(pacientes)
+    console.table(pacientes)
+    console.log(pacientes[0].nombre)
+    console.log(pacientes.pop())
+    console.table(pacientes)
+
+    //desestructuracion de arreglos
+    const diasSemana2 = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+    const [dia1, dia2, dia3, dia4, dia5] = diasSemana2
+    console.log(dia1)
+    console.log(dia5)
+
+    const [d1, d2, d3, d4, d5] = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes"]
+    console.log(dia2)
+    console.log(dia4)
+
+    //desestructuracion de objetos
+    const vehiculo = { marca: "Nissan", modelo: "Modelo1", anio: "2022" }
+    const { marca, modelo, anio } = vehiculo;
+    console.log(modelo)
+    console.log(anio)
+    console.log(marca)
+    ////////////////////////////////
+    const { nombre, edad, direccion: { callePrincipal, calleSecundaria, numeracion } } = {
+        nombre: "Daniel",
+        edad: 25,
+        direccion: {
+            callePrincipal: "America",
+            calleSecundaria: "Bolivia",
+            numeracion: "Oe3-004",
+        }
+    }
+    console.log(nombre)
+    console.log(callePrincipal)
+
 }

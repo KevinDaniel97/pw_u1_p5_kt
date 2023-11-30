552f8e59a3283cf444f6bac9bb90d99e28dd5c7d
carolf10 <fcarofialloscheca@hotmail.com>
2023-11-29 19:45:50 -0500

    taller10

--- js/calculadoraOperaciones.js
+++ js/calculadoraOperaciones.js
@@ -54,19 +54,19 @@ let resultado = '';
 
 function calc(num) {
     resultado += num;
-    document.getElementById('labelResultado').innerHTML = "" + resultado;
+    document.getElementById('labelResultado').innerHTML = 'Resultado: ' + resultado;
     console.log(resultado)
 }
 
 function calcClear() {
     resultado = ''
-    document.getElementById('labelResultado').innerHTML = "";
+    document.getElementById('labelResultado').innerHTML = 'Resultado: ';
 }
 
 function calcRes() {
 
     let resNumerico = parseFloat(eval(resultado))
-    document.getElementById('labelResultado').innerHTML = resNumerico;
+    document.getElementById('labelResultado').innerHTML = 'Resultado: '+resNumerico;
     console.log(resNumerico)
     resultado = resNumerico
 

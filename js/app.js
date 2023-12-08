065c946910a73c114b2fda82c9c24d3e7ce11492
carolf10 <fcarofialloscheca@hotmail.com>
2023-12-07 19:46:03 -0500

    taller13

---
+++ js/app.js
@@ -0,0 +1,15 @@
+console.log('elemnetos Vue')
+console.log(Vue)
+
+const app = Vue.createApp({
+    // template: `
+    // <h1> Hola Mundo </h1>
+    // <p> Desde App.js</p>
+    // <h6>{{1+1}}</h6>
+    // <p>{{[1,2,3,4,5,6,7,8]}}</p>
+    // <p>{{true?'Verdadero':'Falso'}}</p>
+    // `
+
+})
+
+app.mount('#myApp')

deb42864d052fe1177f10c402d06cf4639348d52
carolf10 <fcarofialloscheca@hotmail.com>
2023-12-09 10:39:24 -0500

    taller14

--- js/app.js
+++ js/app.js
@@ -9,7 +9,27 @@ const app = Vue.createApp({
     // <p>{{[1,2,3,4,5,6,7,8]}}</p>
     // <p>{{true?'Verdadero':'Falso'}}</p>
     // `
+    methods: {
+        cambiarMensaje() {//parentecis no es necesario si no hay argumentos
+            console.log('se esta cambiando el mensaje')
+            console.log(this.mensaje)
+            mensaje = 'valor cambiado'
+        },
+        cambiarNumero() {
+            this.valor++
+            console.log(this.valor)
+        }
+    },
+    watch: {
 
+    },
+    data() {
+        return {
+            mensaje: 'hola mundo desde Vue.JS',
+            valor: 100
+        }
+    }
 })
+
 
 app.mount('#myApp')
